6843403e574cd75d2c99dff91f831fa3acc0ff5d
OneLine <oneline1821@gmail.com>
2019-09-06 09:04:23 -0500

    pushhh

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,20 @@ export const environment = {
   production: false,
   version: '1.0',
 
-  serverRecursos: 'http://200.60.83.164',
-  serverRutas: 'http://200.60.83.164/api',
+  // serverRecursos: 'http://200.60.83.164',
+  // serverRutas: 'http://200.60.83.164/api',
 
   // serverRecursos: 'http://172.16.0.145',
   // serverRutas: 'http://172.16.0.145/api',
+
   //serverRecursos: 'http://200.60.83.164',
   //serverRutas: 'http://200.60.83.164/api',
+
+  serverRecursos: 'http://127.0.0.1:8000',
+  serverRutas: 'http://127.0.0.1:8000/api',
+
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
